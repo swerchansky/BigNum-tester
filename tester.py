f463451d17fd2d24a09dd47875e5b1a4833d654f
Tarchevsky Alexey <swerch.a@gmail.com>
2022-06-04 23:15:43 +0300

    tester fix + random tests

--- tester.py
+++ tester.py
@@ -10,15 +10,19 @@ from colorama import Style
 colorama.init()
 
 d = {0: "git tests", 1: "plus tests", 2: "minus tests", 3: "mult tests", 4: "div tests", 5: "module tests",
-     6: "sqrt tests", 7: "compression tests", 8: "random tests"}
+     6: "sqrt tests", 7: "compare tests", 8: "random tests"}
+
+count = 0
+total = 0
 
 for j in range(len(d)):
     print(Back.MAGENTA + f"{d[j]}\n")
     for i in range(len(list(Path(f"samples/{d[j]}/").iterdir()))):
+        total += 1
         print(Back.CYAN + f"test {i}: ", end="")
-        subprocess.call(["java", "-jar", "tester.jar", f"samples/{d[j]}/in{i}.txt", f"samples/{d[j]}/out_ref{i}.txt"])
+        subprocess.call(["java", "-jar", "tester.jar", f"samples/{d[j]}/in{i}.txt", f"out_ref{i}.txt"])
         subprocess.run([sys.argv[1], f"samples/{d[j]}/in{i}.txt", f"out{i}.txt"])
-        with open(f"samples/{d[j]}/out_ref{i}.txt") as first, open(f"out{i}.txt") as second:
+        with open(f"out_ref{i}.txt") as first, open(f"out{i}.txt") as second:
             fl = True
             pairs = list(zip(first.readlines(), second.readlines()))
             cnt = 1
@@ -32,12 +36,14 @@ for j in range(len(d)):
             if fl:
                 print(Back.GREEN + "Passed", end="")
                 print(Style.RESET_ALL)
+                count += 1
             else:
                 print(Back.RED + "Failed", end="")
                 print(Style.RESET_ALL)
 
         os.remove(f"out{i}.txt")
-        if fl:
-            os.remove(f"samples/{d[j]}/out_ref{i}.txt")
+        os.remove(f"out_ref{i}.txt")
     print()
+
+print(Back.MAGENTA + f"Total count: {count}/{total}")
 
