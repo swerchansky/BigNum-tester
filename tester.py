a834833f520cdc561a2ba8b14b576eafbedd244f
Tarchevsky Alexey <swerch.a@gmail.com>
2022-06-05 22:17:42 +0300

    custom directory added

--- tester.py
+++ tester.py
@@ -15,16 +15,56 @@ d = {0: "git tests", 1: "plus tests", 2: "minus tests", 3: "mult tests", 4: "div
 count = 0
 total = 0
 
-for j in range(len(d)):
-    print(Back.MAGENTA + f"{d[j]}\n")
-    ls = os.listdir(f"samples/{d[j]}/")
+if len(sys.argv) == 2:
+    for j in range(len(d)):
+        print(Back.MAGENTA + f"{d[j]}\n")
+        ls = os.listdir(f"samples/{d[j]}/")
+        for i in range(len(ls)):
+            try:
+                total += 1
+                print(Back.CYAN + f"test {ls[i]}: ", end="")
+                st = str(ls[i])
+                subprocess.call(["java", "-jar", "tester.jar", f"samples/{d[j]}/" + st, f"out_ref.txt"])
+                subprocess.run([sys.argv[1], f"samples/{d[j]}/" + st, f"out.txt"])
+                with open(f"out_ref.txt") as first, open(f"out.txt") as second:
+                    fl = True
+                    pairs = list(zip(first.readlines(), second.readlines()))
+                    cnt = 1
+                    for pair in pairs:
+                        line1 = pair[0].rstrip()
+                        line2 = pair[1].rstrip()
+                        if line1 != line2:
+                            fl = False
+                            break
+                        cnt += 1
+                    if fl:
+                        print(Back.GREEN + "Passed", end="")
+                        print(Style.RESET_ALL)
+                        count += 1
+                    else:
+                        print(Back.RED + "Failed", end="")
+                        print(Style.RESET_ALL)
+
+                os.remove(f"out.txt")
+                os.remove(f"out_ref.txt")
+            except FileNotFoundError:
+                print(Back.RED + "Выходной файл не создался (программа упала)", end="")
+                print(Style.RESET_ALL)
+                os.remove(f"out_ref.txt")
+        print()
+
+    print(Back.MAGENTA + f"Total count: {count}/{total}")
+
+else:
+    print(Back.MAGENTA + f"{sys.argv[2]}\n")
+    ls = os.listdir(f"samples/{sys.argv[2]}/")
     for i in range(len(ls)):
         try:
             total += 1
             print(Back.CYAN + f"test {ls[i]}: ", end="")
             st = str(ls[i])
-            subprocess.call(["java", "-jar", "tester.jar", f"samples/{d[j]}/" + st, f"out_ref.txt"])
-            subprocess.run([sys.argv[1], f"samples/{d[j]}/" + st, f"out.txt"])
+            subprocess.call(["java", "-jar", "tester.jar", f"samples/{sys.argv[2]}/" + st, f"out_ref.txt"])
+            subprocess.run([sys.argv[1], f"samples/{sys.argv[2]}/" + st, f"out.txt"])
             with open(f"out_ref.txt") as first, open(f"out.txt") as second:
                 fl = True
                 pairs = list(zip(first.readlines(), second.readlines()))
@@ -53,4 +93,3 @@ for j in range(len(d)):
     print()
 
 print(Back.MAGENTA + f"Total count: {count}/{total}")
-
